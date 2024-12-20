dad9c07a5df18b73b55913e4995d02e9ab89ea0d
lordace-coder <lordyacey@gmail.com>
2024-12-20 02:31:19 +0100

    changes

--- lib/bloc/posts/post_cubit.dart
+++ lib/bloc/posts/post_cubit.dart
@@ -4,31 +4,56 @@ import 'package:pocketbase/pocketbase.dart';
 
 class PostCubit extends Cubit<PostState> {
   final PocketBase _pocketBase;
+  int _currentPage = 1;
+  List<Post> _currentPosts = [];
+  static const int _perPage = 30;
 
   PostCubit(this._pocketBase) : super(PostInitial());
 
-  Future<void> fetchPosts() async {
-    emit(PostLoading());
+  Future<void> fetchPosts({bool loadMore = false}) async {
+    if (!loadMore) {
+      emit(PostLoading());
+      _currentPage = 1;
+      _currentPosts = [];
+    }
 
     try {
-      final records = await _pocketBase
-          .collection('posts')
-          .getList(expand: 'community', sort: '-created');
-      emit(PostLoaded(
-        records.items
-            .map((record) => Post.fromRecord(record, _pocketBase))
-            .toList(),
-      ));
+      final records = await _pocketBase.collection('posts').getList(
+            expand: 'community',
+            sort: '-created',
+            page: _currentPage,
+            perPage: _perPage,
+          );
+
+      final newPosts = records.items
+          .map((record) => Post.fromRecord(record, _pocketBase))
+          .toList();
+
+      if (loadMore) {
+        _currentPosts.addAll(newPosts);
+      } else {
+        _currentPosts = newPosts;
+      }
+
+      _currentPage++;
+
+      emit(PostLoaded(_currentPosts));
     } catch (e) {
       emit(PostError(e.toString()));
     }
   }
 
+  Future<void> loadMorePosts() async {
+    if (state is PostLoaded) {
+      await fetchPosts(loadMore: true);
+    }
+  }
+
   Future<void> createPost(Map<String, dynamic> data) async {
     emit(PostLoading());
-
     try {
       await _pocketBase.collection('posts').create(body: data);
+      _currentPage = 1;
       await fetchPosts(); // Refresh the post list
     } catch (e) {
       emit(PostError(e.toString()));
@@ -38,7 +63,7 @@ class PostCubit extends Cubit<PostState> {
   Future<void> updatePost(String id, Map<String, dynamic> data) async {
     try {
       await _pocketBase.collection('posts').update(id, body: data);
-      // Refresh the post list
+      await fetchPosts(); // Refresh the post list
     } catch (e) {
       emit(PostError(e.toString()));
     }
@@ -46,7 +71,6 @@ class PostCubit extends Cubit<PostState> {
 
   Future<void> deletePost(String id) async {
     emit(PostLoading());
-
     try {
       await _pocketBase.collection('posts').delete(id);
       await fetchPosts(); // Refresh the post list
@@ -55,9 +79,7 @@ class PostCubit extends Cubit<PostState> {
     }
   }
 
-  Future<void> likePost(
-    String id,
-  ) async {
+  Future<void> likePost(String id) async {
     await updatePost(id, {
       'likes+': [_pocketBase.authStore.model.id]
     });
