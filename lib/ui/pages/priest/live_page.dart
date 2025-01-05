2f22ec2cb9a2d2829b864ee0792a695d066b6830
lordace-coder <lordyacey@gmail.com>
2025-01-05 02:11:59 +0100

    added audio live room

--- lib/ui/pages/priest/live_page.dart
+++ lib/ui/pages/priest/live_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:oratio_app/bloc/profile_cubit/profile_data_cubit.dart';
+import 'package:oratio_app/networkProvider/priest_requests.dart';
+import 'package:pocketbase/pocketbase.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:web_socket_channel/status.dart' as status;
+import 'package:zego_uikit_prebuilt_live_audio_room/zego_uikit_prebuilt_live_audio_room.dart';
 
 class LiveMassPage extends StatefulWidget {
   LiveMassPage({super.key, required this.parishId, this.isPriest = false});
@@ -70,6 +75,29 @@ class _LiveMassPageState extends State<LiveMassPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final user =
+        getPocketBaseFromContext(context).authStore.model as RecordModel;
+
+    return Scaffold(
+      body: ZegoUIKitPrebuiltLiveAudioRoom(
+        appID: 2015132394,
+        appSign:
+            "c905dd2394441c4d848c59d16f1505781e1366ecef028e22cc73b3c7010e141f", // Fill in the appSign that you get from ZEGOCLOUD Admin Console.
+        userID: user.id,
+        userName: user.getStringValue('username'),
+        roomID: widget.parishId,
+        config: widget.isPriest
+            ? ZegoUIKitPrebuiltLiveAudioRoomConfig.host()
+            : ZegoUIKitPrebuiltLiveAudioRoomConfig.audience()
+          ..turnOnMicrophoneWhenJoining = false
+          ..bottomMenuBar = widget.isPriest
+              ? ZegoLiveAudioRoomBottomMenuBarConfig(
+                  audienceButtons: [],
+                )
+              : ZegoLiveAudioRoomBottomMenuBarConfig(
+                  audienceButtons: [],
+                ),
+      ),
+    );
   }
 }
